478fa5f45f9943fae6df15509bf295ae8ef159c2
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-08-23 20:50:54 -0300

    feat: sobe projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  axios
+    .get("https://api.github.com/users/KeysiJones")
+    .then((res) => console.log(res));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="bg-gray-700 h-full p-6">
+      <div className="items-center flex flex-col text-center">
+        <a href="https://github.com/keysijones">
+          <img
+            alt="opa"
+            className="rounded-full"
+            style={{ height: "170px" }}
+            src="https://avatars.githubusercontent.com/u/55116367?v=4"
+          />
         </a>
-      </header>
+        <p className="text-white text-2xl mt-2">Keysi Jones</p>
+        <p className="text-white text-xl mt-2">Porto Alegre, Brasil</p>
+        <p className="text-white text-sm mt-2">*No GitHub desde 10/09/2019</p>
+      </div>
+      <div className="text-white border-2 border-gray-400 p-4 rounded-xl w-9/12 text-center mx-auto mt-12 grid">
+        <p className="text-xl">About me</p>
+        <p className="mt-2 text-md">
+          Java Backend Software Engineer Passionate about technology, especially
+          programming. I like to code, to ride a bike, to be with my family...
+          and etc..
+        </p>
+      </div>
+      <div className="text-white border-2 border-gray-400 p-4 rounded-xl w-9/12 text-center mx-auto mt-12 grid">
+        <p className="text-xl">About me</p>
+        <p className="mt-2 text-md">
+          Java Backend Software Engineer Passionate about technology, especially
+          programming. I like to code, to ride a bike, to be with my family...
+          and etc..
+        </p>
+      </div>
     </div>
   );
 }
